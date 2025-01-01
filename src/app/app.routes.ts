3b2fe745cc344d75d4b1040014206efc817b2fdd
Luca <luca.rauchenberger@gmail.com>
2025-01-01 18:17:32 +0100

    set up routing and removed placeholder default angular design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {ServerOverviewComponent} from './pages/server-overview/server-overview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ServerOverviewComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
